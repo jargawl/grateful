2fb751f3e5342217a1db61117fe0291734c2d6e3
Jaroslaw Gawlik <jargawl@gmail.com>
2022-05-26 23:54:06 +0200

    Implement firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,15 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+admin.initializeApp();
+
+exports.createUserRecord = functions.auth.user().onCreate((user)=>{console.log('user created',user.email,user.uid);
+return admin.firestore().collection('users').doc(user.uid).set({email: user.email, create_at: admin.firestore.FieldValue.serverTimestamp()}) })
+
+
+exports.deleteUserRecord = functions.auth.user().onDelete((user)=>{console.log('user deleted',user.email,user.uid);
+return admin.firestore().collection('users').doc(user.uid).delete() })
