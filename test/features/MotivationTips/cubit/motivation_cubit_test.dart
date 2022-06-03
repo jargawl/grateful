57e484d3fda6af763c6da4f3f4b19c50dd53c99a
Jaroslaw Gawlik <jargawl@gmail.com>
2022-06-03 23:35:18 +0200

    Finish test MotivationCubit without State.error

--- test/features/MotivationTips/cubit/motivation_cubit_test.dart
+++ test/features/MotivationTips/cubit/motivation_cubit_test.dart
@@ -1,6 +1,8 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:thankfulness/App/core/enums.dart';
 import 'package:thankfulness/features/MotivationTips/cubit/motivation_cubit.dart';
+import 'package:thankfulness/features/MotivationTips/model/motivation_model.dart';
 import 'package:thankfulness/features/MotivationTips/repositories/motivation_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -15,11 +17,51 @@ void main() {
     sut = MotivationCubit(motivationRepository: repository);
   });
   group('start', () {
+    setUp(() {
+      when(() => repository.getMotivationModels()).thenAnswer(
+        (_) async => [
+          MotivationModel(
+              1,
+              'Życie to bezcenny dar, który otrzymałeś i powinieneś być za niego wdzięczny',
+              'Twoje życie'),
+          MotivationModel(
+              2,
+              'Każdy dzień kiedy dopisuje Ci zdrowie jest cenny. Doceniaj swoje ciało, swoją sprawność. Wiele osób, zaczyna myśleć o wartości zdrowia dopiero, gdy je stracą i dopada ich choroba.',
+              'Dobre zdrowie'),
+          MotivationModel(
+              3,
+              'Jeśli masz w życiu osoby, które możesz nazwać swoimi przyjaciółmi to jest bardzo cenne.',
+              'Przyjaciele'),
+        ],
+      );
+    });
+
     blocTest<MotivationCubit, MotivationState>(
-      'description',
+      'emits status.loading then Status.success with results ',
       build: () => sut,
       act: (cubit) => cubit.start(),
-      expect: () => [],
+      expect: () => [
+        MotivationState(
+          status: Status.loading,
+        ),
+        MotivationState(
+          status: Status.success,
+          results: [
+            MotivationModel(
+                1,
+                'Życie to bezcenny dar, który otrzymałeś i powinieneś być za niego wdzięczny',
+                'Twoje życie'),
+            MotivationModel(
+                2,
+                'Każdy dzień kiedy dopisuje Ci zdrowie jest cenny. Doceniaj swoje ciało, swoją sprawność. Wiele osób, zaczyna myśleć o wartości zdrowia dopiero, gdy je stracą i dopada ich choroba.',
+                'Dobre zdrowie'),
+            MotivationModel(
+                3,
+                'Jeśli masz w życiu osoby, które możesz nazwać swoimi przyjaciółmi to jest bardzo cenne.',
+                'Przyjaciele'),
+          ],
+        )
+      ],
     );
   });
 }
