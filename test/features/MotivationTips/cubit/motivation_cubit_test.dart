20ba4e4dbe02024254f0082df49a925ac9f2d506
Jaroslaw Gawlik <jargawl@gmail.com>
2022-06-03 23:04:40 +0200

    prepre BlocTest MotivationCubit

--- test/features/MotivationTips/cubit/motivation_cubit_test.dart
+++ test/features/MotivationTips/cubit/motivation_cubit_test.dart
@@ -1,6 +1,25 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:thankfulness/features/MotivationTips/cubit/motivation_cubit.dart';
 import 'package:thankfulness/features/MotivationTips/repositories/motivation_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockMotivationRepository extends Mock implements MotivationRepository {}
 
-void main() {}
+void main() {
+  late MotivationCubit sut;
+  late MockMotivationRepository repository;
+
+  setUp(() {
+    repository = MockMotivationRepository();
+    sut = MotivationCubit(motivationRepository: repository);
+  });
+  group('start', () {
+    blocTest<MotivationCubit, MotivationState>(
+      'description',
+      build: () => sut,
+      act: (cubit) => cubit.start(),
+      expect: () => [],
+    );
+  });
+}
