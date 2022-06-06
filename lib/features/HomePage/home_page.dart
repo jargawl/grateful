1d3e218b0f7c7c16d47f71b57a81fe057fb741fc
Iwona Rudzinska <rudzinska.iwona96@gmail.com>
2022-06-06 13:47:42 +0200

    Add ResponsiveLayout

--- lib/features/HomePage/home_page.dart
+++ lib/features/HomePage/home_page.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:rive/rive.dart';
-import 'package:thankfulness/App/core/config.dart';
-import 'package:thankfulness/features/AppPages/Goals/goals.dart';
-import 'package:thankfulness/features/Auth/pages/user_profile.dart';
-import 'package:thankfulness/models/Widgets/goals/goals_counter.dart';
-import 'package:thankfulness/models/Widgets/grateful/grateful_counter.dart';
-import 'package:thankfulness/models/Widgets/motivationButton/motivation_button.dart';
-import 'package:thankfulness/features/MotivationTips/motivation.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-import '../AppPages/Grateful/grateful.dart';
+import 'package:thankfulness/App/core/responsive_layout.dart';
+import 'package:thankfulness/features/HomePage/narrow_home_page.dart';
+import 'package:thankfulness/features/HomePage/ultra_wide_home_page.dart';
+import 'package:thankfulness/features/HomePage/wide_home_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
@@ -19,88 +11,10 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const UserProfile(),
-                  ),
-                );
-              },
-              icon: const Icon(
-                Icons.person,
-              ),
-              color: Colors.black,
-              iconSize: 35,
-            )
-          ],
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(25),
-            ),
-          ),
-          title: Center(
-            child: Text(
-              (AppLocalizations.of(context)!.titleGrateful),
-              semanticsLabel: Config.title,
-              style: GoogleFonts.pacifico(
-                color: const Color.fromARGB(255, 23, 213, 169),
-                fontSize: 25,
-              ),
-            ),
-          ),
-        ),
-        backgroundColor: const Color.fromARGB(255, 216, 228, 237),
-        drawer: Drawer(
-          backgroundColor: const Color.fromARGB(255, 216, 228, 237),
-          child: ListView(
-            children: const [
-              Grateful(),
-              Goals(),
-            ],
-          ),
-        ),
-        body: Column(
-          children: [
-            const SizedBox(
-              height: 5,
-            ),
-            const NameWidgetAchive('Twoja ilość powodów do wdzięczności'),
-            const SizedBox(
-              height: 5,
-            ),
-            const GratefulCounter(),
-            const NameWidgetGrateful('Twoja liczba celów do realizowania'),
-            const SizedBox(
-              height: 5,
-            ),
-            const GoalsCounter(),
-            const SizedBox(
-              height: 5,
-            ),
-            const MotivationButton(),
-            Expanded(
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => const MotivationPage(),
-                    ),
-                  );
-                },
-                child: const RiveAnimation.asset(
-                  'assets/a.riv',
-                  alignment: Alignment.bottomCenter,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+    return const ResponsiveLayout(
+      ultrawide: UltraWideHomePage(),
+      wide: WideHomePage(),
+      narrow: NarrowHomePage(),
     );
   }
 }
